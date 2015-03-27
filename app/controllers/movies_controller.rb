5e2cf0e770a29cbfd0fc944919c274b7c7077181
Ali Arslan <ali_arslan@hotmail.com>
2015-03-28 00:00:27 +0500

    hi

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,73 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+
+    @all_ratings = Movie.all_ratings
+    ratings_hash = params[:ratings]
+
+    @selected_ratings = @all_ratings if @selected_ratings.nil?
+      
+      # if params(:sort) != nil
+   @sort_by = params[:sort_by]
+
+    
+    if cookies[:ratings]!=nil
+      ratings_pers = YAML::load cookies[:ratings]
+      sort_pers = YAML::load cookies[:sort]
+    end
+
+    if sort_pers == nil and @sort_by !=nil
+      cookies[:sort] = @sort_by
+      sort_pers = @sort_by
+      flash.keep
+      redirect_to movies_path(@sort_by)
+    elsif @sort_by == nil and sort_pers !=nil
+      @sort_by = sort_pers
+      cookies[:sort] = @sort_by
+    elsif @sort_by != sort_pers and @sort_by != nil and sort_pers != nil
+      sort_pers = @sort_by
+      cookies[:sort] = @sort_by
+    end
+
+    if ratings_pers == nil and ratings_hash !=nil 
+      cookies[:ratings] = YAML::dump ratings_hash
+      ratings_pers = ratings_hash
+      flash.keep
+      redirect_to movies_path(ratings_hash)
+    elsif ratings_hash == nil and ratings_pers !=nil 
+      ratings_hash = ratings_pers
+      cookies[:ratings] = YAML::dump ratings_hash
+    elsif ratings_hash != ratings_pers and ratings_hash!= nil and ratings_pers!= nil 
+      ratings_pers = ratings_hash
+      cookies[:ratings] = YAML::dump ratings_hash
+    end
+
+    if ratings_hash != nil
+      @selected_ratings = ratings_hash.keys
+      extracted = ratings_hash.select{|key, hash| hash == "1"}.map{|i| i[0]}
+      newMovs = []
+      (0..extracted.size - 1).each do |i|
+        x = Movie.find(:all,:conditions =>{:rating => extracted[i]})
+        (0..x.size - 1).each do|j|
+          newMovs.push(x[j])
+        end
+      end
+      if @sort_by == "title"
+        @movies = newMovs.sort!{|a,b| a.title <=> b.title}
+      end
+      if @sort_by == "release_date"
+        @movies = newMovs.sort!{|a,b| a.release_date <=> b.release_date}
+      end
+      if @sort_by == nil
+        @movies = newMovs
+      end
+    else
+      begin
+        @movies = Movie.order("#{@sort_by} ASC").find(:all)
+      rescue ActiveRecord::StatementInvalid
+      end
+    end
+
   end
 
   def show
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
+
+   @@all_ratings = ["G","PG-13","PG","R","NC-17"]
+
+   cattr_accessor :all_ratings
+
 end
